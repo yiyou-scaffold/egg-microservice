c8a8d81dbf8b08d188e0475c2c546a491be848f9
yushicheng <1542874601@qq.com>
2021-03-11 19:22:03 +0800

    temp:临时节点标记 星期四 晚上 7:22:03 三月 11日 2021 修改了 deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -3,6 +3,10 @@
 const path = require('path');
 const { name } = require('./package.json');
 
+/**
+ * @link https://yiyou-static.oss-cn-hangzhou.aliyuncs.com/{name}/docs/index.html
+ * **/
+
 module.exports = {
   local_dist: path.resolve(process.cwd(), './docs/**/*.*'),
   remote_bucket: name,
